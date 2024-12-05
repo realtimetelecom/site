f066d3396e3aab996f858022686159517429bd15
realtimetelecom <idiogo.lima@gmail.com>
2024-12-05 08:02:35 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,3 +10,20 @@ function showSolutions(category) {
 function scrollToCategories() {
     document.getElementById('categories').scrollIntoView({ behavior: 'smooth' });
 }
+
+function toggleTheme() {
+    const body = document.body;
+    const toggleIcon = document.querySelector("#theme-toggle i");
+
+    // Alterna a classe 'dark-theme' no body
+    body.classList.toggle("dark-theme");
+
+    // Altera o ícone do botão com base no tema atual
+    if (body.classList.contains("dark-theme")) {
+        toggleIcon.classList.remove("fa-moon");
+        toggleIcon.classList.add("fa-sun");
+    } else {
+        toggleIcon.classList.remove("fa-sun");
+        toggleIcon.classList.add("fa-moon");
+    }
+}
